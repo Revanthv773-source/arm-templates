abdf676ed6273e45aa38a69d3b587a53b1b554f5
revanth <revanthv73@gmail.com>
2021-07-18 15:18:54 +0530

    second commit

--- arm templates/Controllers/WeatherForecastController.cs
+++ arm templates/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace arm_templates.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balamy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
